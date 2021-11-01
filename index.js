092234cf42200cf1f582d533586df1b657d8f0cd
ALG0GA <69913585+ALG0GA@users.noreply.github.com>
2021-11-01 20:36:28 +0100

    init

---
+++ index.js
@@ -0,0 +1,74 @@
+const Discord = require('discord.js');
+const CronJob = require('cron').CronJob;
+
+
+
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+                            partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+});
+const roleId = "898944919018803200";
+
+client.once('ready', () => {
+
+    var job = new CronJob('0 23 * * 1', function() {
+        let calendarChannel = client.channels.cache.get("901416311421284372");
+        let date = new Date;
+    
+        calendarChannel.bulkDelete(3)
+        .then(messages => console.log(`Bulk deleted ${messages.size} messages`))
+        .catch(console.error);
+    
+        calendarChannel.send("<@&"+roleId+"> Raid de **mercredi** "+getNextDayOfWeek(date,3)+":");     
+        calendarChannel.send("<@&"+roleId+"> Raid de **dimanche** "+getNextDayOfWeek(date,7)+":");
+        calendarChannel.send("<@&"+roleId+"> Raid de **lundi** "+getNextDayOfWeek(date,8)+":");      }, null, true, 'Europe/Paris');
+      job.start();
+        
+    
+
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+	// When a reaction is received, check if the structure is partial
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message:', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+	}
+    if (reaction.message.content.includes('mercredi')){
+        console.log(reaction.message.author.username, reaction.emoji.name);
+    }
+    else if(reaction.message.content.includes('dimanche')){
+        console.log(reaction.message.author.username, reaction.emoji.name);
+    }
+    else if (reaction.message.content.includes('lundi')){
+        console.log(reaction.message.author.username, reaction.emoji.name);
+    }
+	// // Now the message has been cached and is fully available
+	// console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+	// // The reaction is now also fully available and the properties will be reflected accurately:
+	// console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+});
+
+function getNextDayOfWeek(date, dayOfWeek) {
+    // Code to check that date and dayOfWeek are valid left as an exercise ;)
+
+    var resultDate = new Date(date.getTime());
+
+    resultDate.setDate(date.getDate() + (7 + dayOfWeek - date.getDay()) % 7);
+    console.log(resultDate.getFullYear());
+
+    return format(resultDate.getDate(),resultDate.getMonth(),resultDate.getFullYear());
+}
+
+function format(day,month,year){
+    return day+ "/"+month+"/"+year;
+}
+client.login(process.env.TOKEN);
+
